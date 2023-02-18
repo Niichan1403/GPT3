4d867503cd037922cff6bca6506b3d7605d3be3d
Trần Tuấn Anh <tuananhniichan@gmail.com>
2023-02-18 17:08:49 +0700

    Brand & WhatGPT3
    Section implemented: Brand -> WhatGPT3

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <div className="gradient__bg">
+    <div className="App gradient__bg">
+      <div className="">
         <Navbar />
         <Header />
       </div>
